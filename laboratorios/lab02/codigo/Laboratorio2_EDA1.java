415be66826d22bc1b58419570dbc9f48b261b1a2
SaraRodriguezV <sarisrove@hotmail.com>
2020-02-26 18:26:01 -0500

    Taller6

---
+++ laboratorios/lab02/codigo/Laboratorio2_EDA1.java
@@ -0,0 +1,174 @@
+﻿/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laboratorio2_eda1;
+import java.util.Random;
+/**
+ *
+ * @author ASUS
+ */
+public class Laboratorio2_EDA1 {
+
+    // Computer the sum of an array
+  public static int ArraySum(int[] A)
+  {          
+      return ArraySumAux(A,0);
+  }
+
+  public static int ArraySumAux(int[] A, int i)
+  {
+    if (i == A.length)
+    {
+      return 0;
+    }
+    return (A[i] + ArraySumAux(A, i+1));
+  }
+  
+  // Computes the maximum value of an array
+  public static int ArrayMax(int[] A)
+  {
+    return ArrayMaxAux(A,A.length-1);
+  }
+
+  public static int ArrayMaxAux(int[] array, int i)
+  {
+    int max, temp;
+        max=array[i];
+        if(i==0) {
+            max=array[i];
+        }
+        else
+        {
+            temp = ArrayMaxAux(array, i-1);
+            if(temp>max) 
+                max=temp;
+        }
+    return max;
+  }
+   
+  // Sorts an array using Insertion Sort
+  public static void InsertionSort(int[] A)
+  {
+        int n = A.length; 
+        for (int i = 1; i < n; ++i) { 
+            int key = A[i]; 
+            int j = i - 1; 
+
+               // Mover los elementos de A más grandes que key una posición más de la actual
+
+            while (j >= 0 && A[j] > key) { 
+                A[j + 1] = A[j]; 
+                j = j - 1; 
+            } 
+            A[j + 1] = key; 
+        } 
+  }
+
+      // Sorts an array using Merge Sort
+      // Taken from www.cs.cmu.edu/
+      
+      public static void mergeSort(int [ ] a)
+      {
+          mergeSort(a, a.length);
+        }
+      
+
+    public static void mergeSort(int [ ] a, int n)
+    {
+        if (n < 2) {
+            return;
+        }
+        int mid = n / 2;
+        int[] l = new int[mid];
+        int[] r = new int[n - mid];
+ 
+        for (int i = 0; i < mid; i++) {
+            l[i] = a[i];
+        }
+        for (int i = mid; i < n; i++) {
+            r[i - mid] = a[i];
+        }
+        mergeSort(l, mid);
+        mergeSort(r, n - mid);
+ 
+        merge(a, l, r, mid, n - mid);
+        
+    }
+
+    private static void merge(int[ ] a, int[ ] l, int[] r, int left, int right )
+    {
+       int i = 0, j = 0, k = 0;
+       while (i < left && j < right) {
+           if (l[i] <= r[j]) {
+               a[k++] = l[i++];
+            }
+            else {
+                a[k++] = r[j++];
+            }
+        }
+        while (i < left) {
+            a[k++] = l[i++];
+        }
+        while (j < right) {
+            a[k++] = r[j++];
+        }
+    }
+  
+  public static void main(String[] args)
+  {
+      int[] A = {1,5,3,2,6};
+      int[] B = {1,5,3,2};
+      int[] C = {1,5,2,6};
+      int[][] Arrays = {A,B,C};  
+      for (int[] X : Arrays)
+      {        
+        mergeSort(X);
+        InsertionSort(X);     
+      }
+      testInsertion();
+      testMerge();
+  }
+  
+  public static void testInsertion() {  
+      System.out.println("Test Insertion: ");
+        for(int i=8000; i<=80000; i+=2500)
+          {
+            
+            int[] arr = new int[i];
+            Random rnd = new Random();
+            for (int j = 0; j < arr.length; j++) 
+            {
+            arr[j] = rnd.nextInt(1000 - 1 );
+            }
+            long inicio= System.currentTimeMillis();
+            InsertionSort(arr);
+            long fin= System.currentTimeMillis();
+            long tiempo = fin-inicio;
+            System.out.println(i+"-"+(fin-inicio));
+            
+          }
+      }
+  
+  public static void testMerge() {
+        System.out.println("Test Merge: ");
+        for(int i=10000; i<=100000000; i+=3000000)
+          {
+            
+            int[] arr = new int[i];
+            Random rnd = new Random();
+            for (int j = 0; j < arr.length; j++) 
+            {
+            arr[j] = rnd.nextInt(1000 - 1 );
+            }
+            long inicio= System.currentTimeMillis();
+            mergeSort(arr);
+            long fin= System.currentTimeMillis();
+            long tiempo = fin-inicio;
+            System.out.println(i+"-"+(fin-inicio));
+            
+          }
+      }
+    
+}
---
+++ talleres/taller06/MiArrayList.java
@@ -0,0 +1,153 @@
+import java.util.Arrays;
+
+/**
+ * La clase MiArrayList tiene la intención de representar un objeto que simule el comportamiento
+ * de la clase ya implementada "ArrayList"
+ * es claro que no se puede utilizar dicha estructura ya utilizada dentro de este ejercicio. 
+ * Para más información de la clase ArrayList:
+ * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/util/ArrayList.html"> Ver documentacion ArrayList </a>
+ * 
+ * 
+ * @author Mauricio Toro, Andres Paez
+ * @version 1
+ */
+
+public class MiArrayList {
+    private int size;
+    private static final int DEFAULT_CAPACITY = 10;
+    private int elements[]; 
+    
+    /**
+    * El metodo constructor se utiliza para incializar
+    * variables a valores neutros como 0 o null.
+    * El contructor no lleva parámetros en este caso.
+    */
+    public MiArrayList() {
+      size = 0;
+      elements = new int[DEFAULT_CAPACITY];
+    }     
+
+    
+    /**
+    * Tiene la intención de retornar la longitud del objeto
+    * @return longitud del objeto
+    *
+    * El size esta influenciado por las funciones add y del
+    */
+    public int size() {
+      return size;
+    }   
+    
+        /** 
+        * @param e el elemento a guardar
+        * Agrega un elemento e a la última posición de la lista
+        *
+        */   
+       public void add(int e)
+       {
+        try {
+          add(size,e);
+        }
+        catch(Exception Exception) {
+          System.out.println("index out of bounds exception");
+        }
+       }
+   
+       //v1 code. Se repetía código.
+     public void addVersion1(int e) {
+       if(size<DEFAULT_CAPACITY)
+        elements[size+1]=e;
+      else{
+        int[] elements2 = new int[size*2];
+        for(int i=0; i<size;i++){
+          elements2[i]=elements[i];
+        }
+        elements2[size+1]=e;
+        elements = elements2;
+        size++;
+      }
+
+    }    
+    
+    
+    /** 
+    * @param i es un íncide donde se encuentra el elemento posicionado
+    * Retorna el elemento que se encuentra en la posición i de la lista.
+    *
+    */
+    public int get(int i) throws Exception 
+    {
+        if(i<size && i>0)
+          return elements[i];
+        else 
+          throw new Exception("index out of bound exception index = "+i);          
+    }
+    
+    
+    /**
+    * @param index es la posicion en la cual se va agregar el elemento
+    * @param e el elemento a guardar
+    * Agrega un elemento e en la posición index de la lista
+    * 
+    */
+    public void add(int index, int e)throws Exception {
+       if(index > size+1)
+       {
+          throw new Exception("index out of bound exception index = "+index);
+        }
+        else{
+          if(size == elements.length)
+          {
+            int[] elements2 = new int[size*2];
+            
+            for(int i=0; i<size;i++)
+            {
+              elements2[i]=elements[i];
+            }
+              
+            elements = elements2;
+            
+            //corro los elementos necesarios un espacio desde el final hasta el index. Le sumo de una vez el espacio extra.
+            size++;
+            for(int i=size; i<=index;i--)
+            {
+               elements[i+1]=elements[i];
+            }
+            
+            elements[index]=e;
+            
+              }
+          else{
+            size++;
+            for(int i=size; i<=index;i--)
+            {
+                elements[i+1]=elements[i];
+              }
+            
+            elements[index]=e;
+              //size++;
+          }
+        }
+      } 
+     
+
+    /**
+    * @param index es la posicion en la cual se va eliminar el elemento
+    *
+    * ELimina el elemento  en la posición index de la lista
+    *
+    */
+    public void del(int index)throws Exception{
+      if(index >= size)
+      {
+        throw new Exception("index out of bound exception index = "+index);
+      }
+      else
+      {
+        for(int i=size-1;i>=index;i--)
+        {
+          elements[i-1]=elements[i];
+        }
+      }
+    }
+}
