fc336f22b74dc03c4ed934849117bd00f5c4d5ba
SaraRodriguezV <sarisrove@hotmail.com>
2020-03-30 17:45:31 -0500

    Ajustes Entrega2

--- proyecto/codigo/DecisionTree.py
+++ proyecto/codigo/DecisionTree.py
@@ -18,6 +18,8 @@ from pprint import pprint
 
 import time
 
+import csv
+
 # ---------------------------------------------------------------------------------------------------------
 """
     Método auxiliar que retorna el estado de la memoria
@@ -39,10 +41,10 @@ def memory_usage_psutil():
 
 def read_dataset_time(dataseto):
     col_list = [ 4,5,6,7,8,9,10,11,12,13,26,28,29,30,31,32,33,35,36,45,46,47,65,66,67,68,69,10,71,72,73,74,75,76,77 ]
-    #start_time = time.time()
+    start_time = time.time()
     startHeap = memory_usage_psutil()
     df = pd.read_csv(dataseto,sep=';',encoding='UTF-8',usecols=col_list)
-    #read_time = (time.time() - start_time)
+    read_time = (time.time() - start_time)
     finalHeap = memory_usage_psutil() - startHeap
     df = df.rename(columns={"exito": "label"})
     
@@ -53,7 +55,7 @@ def read_dataset_time(dataseto):
     df['puntaje_prof'] = df['puntaje_prof'].fillna(0.0)
     df = df.replace(np.nan, "Desconocido")
     
-    return finalHeap
+    return read_time
   
 
 # ----------------------------------------------------------------------------------------------------------
@@ -201,6 +203,40 @@ print(train_dataSet)
 #training_data = df.values.tolist()
 #print(training_data)
 
+# ---------------------------------------------------------------------------------------------------------
+
+"""
+trabajo con CSV READER
+"""
+
+print("LECTURA COMPARATIVA CON CSV READER")
+
+
+def csv_reading_time(dataseto):
+    data_Reader = []
+    start_time = time.time()
+    with open(dataseto,encoding="utf8") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=';')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                #print(f'Column names are {", ".join(row)}')
+                line_count += 1
+            else:
+                #print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+                data_Reader.append(row)
+                line_count += 1
+        read_time = (time.time() - start_time)
+        print(f'Processed {line_count} lines.')
+        
+        return read_time
+    
+    #print(data_Reader)
+    
+    #print('My list:', *data_Reader, sep='\n- ')
+
+
+
 
 # ---------------------------------------------------------------------------------------------------------
 
@@ -222,6 +258,28 @@ testing_data = df.to_numpy()
 
 test_size= testing_data.size
 
+
+print(csv_reading_time("0_test_balanced_5000.csv"))
+print(csv_reading_time("0_train_balanced_15000.csv"))
+print(csv_reading_time("5_test_balanced_19255.csv"))
+print(csv_reading_time("2_test_balanced_25000.csv"))
+print(csv_reading_time("3_test_balanced_35000.csv"))
+print(csv_reading_time("1_train_balanced_45000.csv"))
+print(csv_reading_time("5_train_balanced_57765.csv"))
+print(csv_reading_time("2_train_balanced_75000.csv"))
+print(csv_reading_time("3_train_balanced_105000.csv"))
+print(csv_reading_time("4_train_balanced_135000.csv"))
+
+print(read_dataset_time("0_test_balanced_5000.csv"))
+print(read_dataset_time("0_train_balanced_15000.csv"))
+print(read_dataset_time("5_test_balanced_19255.csv"))
+print(read_dataset_time("2_test_balanced_25000.csv"))
+print(read_dataset_time("3_test_balanced_35000.csv"))
+print(read_dataset_time("1_train_balanced_45000.csv"))
+print(read_dataset_time("5_train_balanced_57765.csv"))
+print(read_dataset_time("2_train_balanced_75000.csv"))
+print(read_dataset_time("3_train_balanced_105000.csv"))
+print(read_dataset_time("4_train_balanced_135000.csv"))
 
 # ---------------------------------------------------------------------------------------------------------  
 
@@ -246,3 +304,48 @@ def train_test_split(df, test_size):
 random.seed(0)
 #train_df, test_df = train_test_split(df, test_size=20)
 
+# -----------------------------------------------------------------------------------------------------------
+
+
+"""
+    Mira que tan mezclado está el data, si es pure retorna true, mezclado feo es false.
+
+"""
+
+def check_purity(data):
+    
+    #label column tiene todos los , en nuestro caso solo debe haber 0 y 1
+    label_column = data[:, -1]
+    unique_classes = np.unique(label_column)
+
+    if len(unique_classes) == 1:
+        return True
+    else:
+        return False
+
+#print(check_purity(training_data[training_data.punt_lenguaje>50]))
+# Forma de filtrar el data
+    
+ # -----------------------------------------------------------------------------------------------------------
+    
+"""
+    Función que clasifica 
+
+"""
+    
+def classify_data(data):
+    
+    label_column = data[:, -1]
+    unique_classes, counts_unique_classes = np.unique(label_column, return_counts=True)
+
+    index = counts_unique_classes.argmax()
+    classification = unique_classes[index]
+    
+    return classification 
+
+print(classify_data(training_data))
+
+label_column = training_data[:,-1]
+print(np.unique(label_column, return_counts = True))
+
+# print(classify_data(train_dataSet[train_dataSet.punt_biologia>50]))
