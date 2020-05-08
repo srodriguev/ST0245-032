41a90141f5447d805de15175a4ce69c4d84553b6
srodriguev <60025041+srodriguev@users.noreply.github.com>
2020-05-08 10:24:58 -0500

    Lab05

---
+++ laboratorios/lab05/codigo/Triplet.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class Triplet here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Triplet<X, Y, Z> { 
+  public final X x; 
+  public final Y y; 
+  public final Z z;
+  
+  public Triplet(X x, Y y, Z z) { 
+    this.x = x; 
+    this.y = y; 
+    this.z = z;
+  } 
+
+
+  public X getX(){
+     return x;
+  }
+  public Y getY(){
+     return y;
+  }
+  public Z getZ(){
+     return z;
+  }
+} 
